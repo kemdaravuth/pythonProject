31d76ce109b4923c608ecb8d6500ba45bafcb77c
biebervuth@gmail.com <!vuthjr1>
2022-11-25 09:54:45 +0700

    Variables and Types

---
+++ Veriables/float.py
@@ -0,0 +1,9 @@
+# To define a float
+myfloat = 8.6
+print(myfloat)
+
+myfloat = float(6)
+print(myfloat)
+
+mylove = 343.23
+print(mylove)
---
+++ Veriables/int.py
@@ -0,0 +1,10 @@
+# To define an integer
+myint = 5
+print(myint)
+
+number = 9
+print(number)
+
+price = 300
+price = 800
+print(price)
